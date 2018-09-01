90e1f035debb3d24229d9bf0ae6d807390e54618
Marty <marty.kerling@gmail.com>
2018-09-01 16:49:50 -0400

    updated spacing

--- plot2.R
+++ plot2.R
@@ -1,14 +1,12 @@
 setwd('~/exploring_data_analysis')
 
 # Read data
-df <- read.table('household_power_consumption.txt', header=TRUE, sep=';',
-                 na.strings='?')
+df <- read.table('household_power_consumption.txt', header=TRUE, sep=';', na.strings='?')
 df$Date <- as.Date(df$Date, format='%d/%m/%Y')
-df$DateTime <- as.POSIXct(paste(df$Date, df$Time), format='%Y-%m-%d %H:%M:%S')
+df$DateTime <- strptime(paste(df$Date, df$Time), format='%Y-%m-%d %H:%M:%S')
 df <- df[df$Date >= as.Date('2007-02-01') & df$Date <= as.Date('2007-02-02'),]
 
 # Plot to png
 png(filename='plot2.png', width=480, height=480, units='px')
-plot(df$DateTime, df$Global_active_power, type='l', xlab='',
-     ylab='Global Active Power (kilowatts)')
+plot(df$DateTime, df$Global_active_power, type='l', xlab='', ylab='Global Active Power (kilowatts)')
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,18 +1,15 @@
 setwd('~/exploring_data_analysis')
 
 # Read data
-df <- read.table('household_power_consumption.txt', header=TRUE, sep=';',
-                 na.strings='?')
+df <- read.table('household_power_consumption.txt', header=TRUE, sep=';', na.strings='?')
 df$Date <- as.Date(df$Date, format='%d/%m/%Y')
-df$DateTime <- as.POSIXct(paste(df$Date, df$Time), format='%Y-%m-%d %H:%M:%S')
+df$DateTime <- strptime(paste(df$Date, df$Time), format='%Y-%m-%d %H:%M:%S')
 df <- df[df$Date >= as.Date('2007-02-01') & df$Date <= as.Date('2007-02-02'),]
 
 # Plot to png
 png(filename='plot3.png', width=480, height=480, units='px')
-plot(df$DateTime, df$Sub_metering_1, type='l', xlab='',
-     ylab='Energy sub metering')
-lines(x=df$Sub_metering_2, col='red')
-lines(x=df$Sub_metering_3, col='blue')
-legend('topright', lty=1, col=c('black','red','blue'),
-       legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'))
+plot(df$DateTime, df$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering')
+lines(df$DateTime, df$Sub_metering_2, col='red')
+lines(df$DateTime, df$Sub_metering_3, col='blue')
+legend('topright', lty=1, col=c('black','red','blue'), legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'))
 dev.off()
