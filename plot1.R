51e4b42675033560115b7343cf7f6a9db153c4bd
Marty <marty.kerling@gmail.com>
2018-09-01 14:48:55 -0400

    edit file ending and quotes

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@ setwd('~/exploring_data_analysis')
 # Read data
 df <- read.table('household_power_consumption.txt', header=TRUE, sep=';',
                  na.strings='?')
-df$Date <- as.Date(df$Date, format="%d/%m/%Y")
+df$Date <- as.Date(df$Date, format='%d/%m/%Y')
 df <- df[df$Date >= as.Date('2007-02-01') & df$Date <= as.Date('2007-02-02'),]
 
 # Plot to png
